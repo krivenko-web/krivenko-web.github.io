efe39703ad16df3e9ee4449e714b0faf6d11241c
krivenko-web <76041853+krivenko-web@users.noreply.github.com>
2021-07-06 08:29:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//menu
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+//avtomat procentu
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+new WOW().init();
